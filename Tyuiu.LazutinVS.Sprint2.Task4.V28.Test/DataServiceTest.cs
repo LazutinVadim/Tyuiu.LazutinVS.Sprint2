471549704e3e24a8b98b811cd14f6ad074cd7afa
VadimLazutin <Tyuiu.LazutinVS@outlook.com>
2025-10-14 14:09:35 +0500

    Task5

---
+++ Tyuiu.LazutinVS.Sprint2.Task4.V28.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.LazutinVS.Sprint2.Task4.V28.Lib;
+namespace Tyuiu.LazutinVS.Sprint2.Task4.V28.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double y = 6;
+            var res = ds.Calculate(x, y);
+            var wait = 9;
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
---
+++ Tyuiu.LazutinVS.Sprint2.Task4.V28/Program.cs
@@ -0,0 +1,59 @@
+﻿using Tyuiu.LazutinVS.Sprint2.Task4.V28.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Title = "Спринт #3 | Выполнил: Лазутин В. С. | РППб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Создание итогового решения по спринту                             *");
+        Console.WriteLine("* Задание #3                                                              *");
+        Console.WriteLine("* Вариант #3                                                             *");
+        Console.WriteLine("* Выполнил: Лазутин В. С.    | РППб-25-1                                  *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+        Console.WriteLine("***************************************************************************");
+        DataService ds = new DataService();
+        double x, y;
+        Console.WriteLine("* Введите значение X: ");
+
+        x = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("* Введите значение Y: ");
+
+        y = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        if (x < y + 2)
+        {Console.Title = "Спринт #3 | Выполнил: Лазутин В. С. | РППб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Создание итогового решения по спринту                             *");
+        Console.WriteLine("* Задание #3                                                              *");
+        Console.WriteLine("* Вариант #3                                                             *");
+        Console.WriteLine("* Выполнил: Лазутин В. С.    | РППб-25-1                                  *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+        Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.Calculate(x, y));
+        }
+        else
+        {
+            Console.WriteLine(ds.Calculate(x, y));
+        }
+
+    }
+}
