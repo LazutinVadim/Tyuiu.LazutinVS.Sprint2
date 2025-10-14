9bde24582bac2c56a93e6ce33a0896326935f0e4
VadimLazutin <Tyuiu.LazutinVS@outlook.com>
2025-10-14 12:51:04 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.LazutinVS.Sprint1.Task0.V21/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.LazutinVS.Sprint2.Task0.V21.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #3 | Выполнил: Лазутин В. С. | РППб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Создание итогового решения по спринту                             *");
+        Console.WriteLine("* Задание #0                                                              *");
+        Console.WriteLine("* Вариант #21                                                             *");
+        Console.WriteLine("* Выполнил: Лазутин В. С.    | РППб-25-1                                  *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+        Console.WriteLine("***************************************************************************");
+
+        int x = 1705;
+        int y = 775;
+        bool[] res = new bool[6];
+        res = ds.GetCompareOperations(x, y);
+
+        for (int i = 0; i < 6; i++)
+        {
+            Console.WriteLine(res[i]);
+        }
+        Console.ReadKey();
+    }
+}
