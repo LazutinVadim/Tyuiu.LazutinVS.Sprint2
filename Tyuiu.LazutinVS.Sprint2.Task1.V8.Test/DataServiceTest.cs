63eafaf206e352db76d497104298f9233680ce13
VadimLazutin <Tyuiu.LazutinVS@outlook.com>
2025-10-14 13:20:01 +0500

    Task1

---
+++ Tyuiu.LazutinVS.Sprint2.Task1.V8.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.LazutinVS.Sprint2.Task1.V8.Lib;
+
+namespace Tyuiu.LazutinVS.Sprint2.Task1.V8.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            bool[] res = new bool[6];
+            int a = 1;
+            int b = 4;
+            int c = 3;
+            int d = 10;
+            bool[] wait = new bool[6] { true, false, true, false, false, false };
+            res = ds.GetLogicOperations(a, b, c, d);
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.LazutinVS.Sprint2.Task1.V8/Program.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.LazutinVS.Sprint2.Task1.V8.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Title = "Спринт #3 | Выполнил: Лазутин В. С. | РППб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Создание итогового решения по спринту                             *");
+        Console.WriteLine("* Задание #0                                                              *");
+        Console.WriteLine("* Вариант #21                                                             *");
+        Console.WriteLine("* Выполнил: Лазутин В. С.    | РППб-25-1                                  *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+        Console.WriteLine("***************************************************************************");
+
+        DataService ds = new DataService();
+        int a = 15;
+        int b = 16;
+        int c = 14;
+        int d = 335;
+        bool[] res = new bool[6];
+        res = ds.GetLogicOperations(a, b, c, d);
+
+        for (int i = 0; i < 6; i++)
+        {
+            Console.WriteLine(res[i]);
+        }
+        Console.ReadKey();
+    }
+}
