e63d4b5f692571717e31e6b5cf4af8fb568f762f
VadimLazutin <Tyuiu.LazutinVS@outlook.com>
2025-10-18 00:35:08 +0500

    Task7

---
+++ Tyuiu.LazutinVS.Sprint2.Task7.V5.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.LazutinVS.Sprint2.Task7.V5.Lib;
+namespace Tyuiu.LazutinVS.Sprint2.Task7.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = -0.4;
+            double y = 0.4;
+            bool wait = true;
+            bool res = ds.CheckDotInShadedArea(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.LazutinVS.Sprint2.Task7.V5/Program.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.LazutinVS.Sprint2.Task7.V5.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        bool res;
+
+        Console.WriteLine("Введите переменную X : ");
+        double x = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите переменную Y : ");
+        double y = Convert.ToDouble(Console.ReadLine());
+        res = ds.CheckDotInShadedArea(x, y);
+
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        if (res)
+        {
+            Console.WriteLine("Точка находится в заштрихованной области");
+        }
+        else
+        {
+            Console.WriteLine("Точка не находится в заштрихованной области");
+        }
+        Console.ReadKey();
+    }
+}
