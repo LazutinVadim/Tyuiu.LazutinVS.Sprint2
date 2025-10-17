c4c302160bc2ddc6a3d2689b45eddcb4d3777070
VadimLazutin <Tyuiu.LazutinVS@outlook.com>
2025-10-18 00:16:26 +0500

    Task6

--- Tyuiu.LazutinVS.Sprint2.Task5.V12.Test/DataServiceTest.cs
+++ Tyuiu.LazutinVS.Sprint2.Task5.V12.Test/DataServiceTest.cs
@@ -1,14 +1,1 @@
-﻿using Tyuiu.LazutinVS.Sprint2.Task5.V12.Lib;
-namespace Tyuiu.LazutinVS.Sprint2.Task5.V12.Test
-{
-    [TestClass]
-    public sealed class DataServiceTest
-    {
-        [TestMethod]
-        public void TestMethod1()
-        {
-            var ds = new DataService(); 
-            Assert.AreEqual("16.10.2024", ds.FindDateOfPreviousDay(2024, 10, 17));
-        }
-    }
-}
+﻿
---
+++ Tyuiu.LazutinVS.Sprint2.Task6.V12.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.LazutinVS.Sprint2.Task5.V12.Lib;
+namespace Tyuiu.LazutinVS.Sprint2.Task5.V12.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            Assert.AreEqual("16.10.2024", ds.FindDateOfPreviousDay(2024, 10, 17));
+        }
+    }
+}
+
