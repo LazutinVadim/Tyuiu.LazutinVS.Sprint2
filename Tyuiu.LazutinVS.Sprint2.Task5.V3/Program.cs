aecbb0320e6c2d7153c24ab7d03d27c913ae48f4
VadimLazutin <Tyuiu.LazutinVS@outlook.com>
2025-10-17 23:22:26 +0500

    Task5

---
+++ Tyuiu.LazutinVS.Sprint2.Task5.V3/Program.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.LazutinVS.Sprint2.Task5.V3.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int num = Convert.ToInt32(Console.ReadLine());
+        DataService ds = new DataService();
+        string res;
+        if ((num < 1) || (num > 7))
+        {
+            res = "Ошибка!";
+        }
+        else
+        {
+            res = ds.FindDayName(num);
+        }
+        Console.WriteLine(res);
+        Console.ReadKey();
+    }
+}
